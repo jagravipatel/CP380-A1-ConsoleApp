450d151bb6277ad5cc12955bf793ed9b147d48fb
Jagravi Patel <jpatel23@confederationcollege.ca>
2021-06-25 10:18:11 -0400

    Update RatingAdjustmentService.cs

--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -22,6 +22,7 @@ namespace RatingAdjustment.Services
         void SetPercentPositive(double stars)
         {
             // TODO: Implement this!
+            _percent_positive = stars / MAX_STARS;
         }
 
         /**
@@ -29,7 +30,8 @@ namespace RatingAdjustment.Services
          */
         void SetQ(double number_of_ratings)
         {
-         // TODO: Implement this!
+            // TODO: Implement this!
+            _q = Z * Math.Sqrt((_percent_positive * (1*_percent_positive) + Z * Z / (4*number_of_ratings)) / number_of_ratings);
         }
 
         /** Adjusted lower bound
@@ -40,7 +42,10 @@ namespace RatingAdjustment.Services
          */
         public double Adjust(double stars, double number_of_ratings) {
             // TODO: Implement this!
-            return 0.0;
+            SetPercentPositive(stars);
+            SetQ(number_of_ratings);
+            double lb = (_percent_positive + (Z * Z / 2 * number_of_ratings) - _q) / (1 + (Z * Z / number_of_ratings));
+            return lb * 5;
         }
     }
 }
